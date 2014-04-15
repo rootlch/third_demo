7d91e392839434ec11dab0468c95df6377de7014
rootlch <louish55@gmail.com>
2014-04-16 07:44:34 +0800

    Change test to use original full_title method

--- spec/support/utilities.rb
+++ spec/support/utilities.rb
@@ -1,8 +1,1 @@
-def full_title(page_title)
-  base_title = "Ruby on Rails Tutorial Sample App"
-  unless page_title.blank?
-    "#{base_title} | #{page_title}"
-  else 
-    base_title
-  end
-end
+include ApplicationHelper
